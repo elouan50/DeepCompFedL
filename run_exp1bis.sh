56b7ae965e643c7bc32248d35b5682d1a94002ba
elouan50 <elouan.colybes@gmail.com>
2025-01-31 11:40:58 +0100

    update automatic run

--- run_exp1bis.sh
+++ run_exp1bis.sh
@@ -2,7 +2,7 @@
 
 for number in 1 2 3
 do
-    for prate in 0.3 0.4 0.5 0.6 0.7 0.8 0.9 0.95 0.99
+    for prate in 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 0.95 0.99
     do
         echo "Experiment with pruning rate $prate"
         flwr run --run-config "
@@ -15,8 +15,8 @@ do
                             fraction-fit=0.1
                             aggregation-strategy='DeepCompFedLStrategy'
                             client-epochs=$epochs
+                            alpha=0.1
                             number=$number
                             "
-                 --federation-config "options.num-supernodes=100"
     done
 done
