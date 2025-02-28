83b4b8f7c78539a6d63ccf024a5196fdce7782bc
elouan50 <elouan.colybes@gmail.com>
2025-02-28 15:10:28 +0100

    updtae exp

--- run_exp2_resnet12.sh
+++ run_exp2_resnet12.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# We want to make a variation on the quantization bits and the number of clients epochs
+# We want to make a variation on:
+# - quantization bits
+# - number of clients epochs
+# - initial space quantization
+
 
 for number in 1 2 3
 do
@@ -8,19 +12,29 @@ do
     do
         for epochs in 1 10
         do
-            echo "Experiment with quantization on $qbits bits and $epochs local epochs."
-            flwr run --run-config "
-                                server-rounds=100
-                                server-enable-quantization=true
-                                server-bits-quantization=$qbits
-                                client-enable-quantization=true
-                                client-bits-quantization=$qbits
-                                model='ResNet12'
-                                fraction-fit=0.4
-                                aggregation-strategy='DeepCompFedLStrategy'
-                                client-epochs=$epochs
-                                number=$number
-                                "
+            for init_space in "random" "density" "uniform"
+            do
+                for lq in "true" "false"
+                do
+                    echo "Experiment with quantization on $qbits bits and $epochs local epochs."
+                    flwr run --run-config "
+                                        server-rounds=100
+                                        server-enable-quantization=true
+                                        server-bits-quantization=$qbits
+                                        client-enable-quantization=true
+                                        client-bits-quantization=$qbits
+                                        model='ResNet12'
+                                        fraction-fit=0.4
+                                        aggregation-strategy='DeepCompFedLStrategy'
+                                        client-epochs=$epochs
+                                        number=$number
+                                        init-space-quantization='$init_space'
+                                        layer-quantization=$lq
+                                        save-online=true
+                                        save-local=true
+                                        "
+                done
+            done
         done
     done
 done
