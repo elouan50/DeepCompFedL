909230838427194c8518c06e944f2aea084a956d
elouan50 <elouan.colybes@gmail.com>
2025-03-07 10:40:35 +0100

    add exp

---
+++ run_exp2_qresnet12.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# We want to make a variation on the quantization and the number of clients epochs
+
+for number in 1 2 3
+do
+    for qbits in 8 4
+    do
+        for epochs in 1 10
+        do
+            echo "Experiment with pruning rate $prate and $epochs local epochs."
+            flwr run --run-config "
+                                server-rounds=100
+                                client-enable-quantization=true
+                                bits-quantization=$qbits
+                                model='QResNet12'
+                                fraction-fit=0.4
+                                aggregation-strategy='DeepCompFedLStrategy'
+                                client-epochs=$epochs
+                                number=$number
+                                alpha=100
+                                save-online=true
+                                save-local=true
+                                "
+        done
+    done
+done
