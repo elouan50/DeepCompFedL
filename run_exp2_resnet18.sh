b8fc9a87125c2e572502f0bbf150b7876dc4841b
elouan50 <elouan.colybes@gmail.com>
2025-03-10 18:15:48 +0100

    exp on batch size

--- run_exp2_resnet18.sh
+++ run_exp2_resnet18.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-# We want to make a variation on the quantization and the number of clients epochs
+# We want to make a variation on the pruning rate.
 
 for number in 1 2 3
 do
-    for qbits in 8 4
+    for qbits in 32 8 4
     do
-        for epochs in 1 10
+        for batch in 8
         do
-            echo "Experiment with pruning rate $prate and $epochs local epochs."
+            echo "Experiment with quantization on $qbits bits and batch size $batch."
             flwr run --run-config "
-                                server-rounds=100
+                                server-rounds=1000
                                 client-enable-quantization=true
                                 bits-quantization=$qbits
-                                model='QResNet12'
-                                fraction-fit=0.4
+                                model='ResNet18'
+                                fraction-fit=0.1
                                 aggregation-strategy='DeepCompFedLStrategy'
-                                client-epochs=$epochs
+                                client-epochs=1
                                 number=$number
                                 alpha=100
-                                batch-size=8
+                                batch-size=$batch
                                 save-online=true
                                 save-local=true
                                 "
