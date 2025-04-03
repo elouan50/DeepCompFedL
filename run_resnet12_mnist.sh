f905eef650e396dab93767f888fff8515fbc9863
elouan50 <elouan.colybes@gmail.com>
2025-04-03 19:04:24 +0200

    add exp

---
+++ run_resnet12_mnist.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# We want to make a variation on the pruning rate and the number of clients epochs
+
+for number in 1 2 3
+do
+    for prate in 0.0 0.2 0.4 0.5 0.6 0.9
+    do
+        for qbits in 32 8 4
+        do
+            for fc in true false
+            do
+                echo "Experiment with pruning rate $prate and $qbits quantization."
+                flwr run --run-config "
+                    server-rounds=50
+                    client-enable-pruning=true
+                    pruning-rate=$prate
+                    client-enable-quantization=true
+                    bits-quantization=$qbits
+                    full-compression=$fc
+                    model='ResNet12'
+                    dataset='MNIST'
+                    fraction-fit=0.4
+                    aggregation-strategy='DeepCompFedLStrategy'
+                    client-epochs=1
+                    number=$number
+                    alpha=100
+                    save-online=true
+                    save-local=true
+                    "
+            done
+        done
+    done
+done
